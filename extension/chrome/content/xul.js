5aa8d3fc0d48b84c94a74277b6a0fa4850af5a0e
Anthony Petty <tpetty@tpetty-laptop.(none)>
2011-03-25 15:58:01 -0400

    Got xul to module communications working.

--- extension/chrome/content/xul.js
+++ extension/chrome/content/xul.js
@@ -1,20 +1,24 @@
-/*
 window.addEventListener("load", function(){
 	hostFileHelper.init();
 }, false);
-*/
+
+var main;
 
 var hostFileHelper = {
-	/**
-	 * Init the app.
-	 */
 	init: function()
 	{
-		this.loadSDKModule("notifications").notify({text: "Hello world!"});
+		//Get a reference to the main module.
+		main = this.loadSDKModule("main");
+	},
+
+	popupShowing: function()
+	{
+		main.myNewFunc();
+		//main.main.editor.show();
 	},
 
 	loadSDKModule: function(module){
-		return Components.classes["@mozilla.org/harness-service;1?id=jid0-i6WjYzrJ0UFR0pPPM7Znl3BvYbk"]
+		return Components.classes["@mozilla.org/harness-service;1?id=jid0-WgoiC7ooIviowYVA4DUnwzlf994"]
 			.getService().wrappedJSObject.loader.require(module);
 	}
 };
--- extension/modules/module.jsm
+++ extension/modules/module.jsm
@@ -1,2 +1,6 @@
 EXPORTED_SYMBOLS = ["exported_test"]
-var exported_test = "foo";
+var exported_test = {
+	init: function(){
+		document.getElementById('hostfilehelper-menu').setAttribute('disabled', true);
+	}
+};
