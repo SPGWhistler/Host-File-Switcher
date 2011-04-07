85c7078cd76a0521685c1e9dcebafb81d7b954fa
Anthony Petty <anthony@anthonypetty.com>
2011-04-06 22:28:42 -0400

    Got basic form updating working.

--- data/hostFileManager/hostFileManager.js
+++ data/hostFileManager/hostFileManager.js
@@ -1,38 +1,53 @@
-//var textArea = document.getElementById('my_text_area');
-var hostFiles;
+var hfm = {
+	//var textArea = document.getElementById('my_text_area');
+	hostFiles : {},
 
-/*
-textArea.onkeyup = function(event){
-	if (event.keyCode === 13)
-	{
-		postMessage(textArea.value);
-		textArea.value = '';
-	}
-};
-*/
-$('#hfm_list').change(this.updateFormElements());
-
-updateFormElements = function(){
-	var myHostFile = $('#hfm_list').val();
-	$('#hfm_hostfile').val(myHostFile);
-	$('#hfm_data').val(hostFiles[myHostFile].data);
-};
-
-self.on('message', function(message){
-	if (typeof message === 'object')
-	{
-		var list_html = '';
-		hostFiles = message;
-		//Clear form elements
-		$('#hfm_list').empty();
-		$('#hfm_hostfile').val('');
-		$('#hfm_data').val('');
-		//Build form data
-		for (var i in hostFiles)
+	/*
+	textArea.onkeyup = function(event){
+		if (event.keyCode === 13)
 		{
-			list_html += '<option value="' + i + '">' + i + '</option>';
+			postMessage(textArea.value);
+			textArea.value = '';
 		}
-		$('#hfm_list').append(list_html);
-		this.updateFormElements();
+	};
+	*/
+
+	init : function()
+	{
+		var self = this;
+		$('#hfm_list').change(function(){
+			//For some reason, I can't call this.updateFormElements directly.
+			//But this seems to work. Also, live and delegate dont work right.
+			self.updateFormElements();
+		});
+		on('message', function(message){
+			if (typeof message === 'object')
+			{
+				var list_html = '';
+				self.hostFiles = message;
+				//Clear form elements
+				$('#hfm_list').empty();
+				$('#hfm_hostfile').val('');
+				$('#hfm_data').val('');
+				//Build form data
+				for (var i in self.hostFiles)
+				{
+					list_html += '<option value="' + i + '">' + i + '</option>';
+				}
+				$('#hfm_list').append(list_html);
+				self.updateFormElements();
+			}
+		});
+	},
+
+	updateFormElements : function()
+	{
+		var myHostFile = $('#hfm_list').val();
+		if (myHostFile !== '' && typeof this.hostFiles[myHostFile] === 'object')
+		{
+			$('#hfm_hostfile').val(myHostFile);
+			$('#hfm_data').val(this.hostFiles[myHostFile].data);
+		}
 	}
-});
+};
+hfm.init();
