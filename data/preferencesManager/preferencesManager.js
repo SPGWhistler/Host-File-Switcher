8f6699b04b9ddd8ee6c481376483e52564e13a85
tpetty <tpetty@synacor.com>
2011-04-07 15:57:09 -0400

    Working on prefs.

--- data/preferencesManager/preferencesManager.js
+++ data/preferencesManager/preferencesManager.js
@@ -1,4 +1,6 @@
 var prefs = {
+	myPrefs : {},
+
 	/**
 	 * Initialize the form.
 	 */
@@ -6,10 +8,13 @@ var prefs = {
 	{
 		var self = this;
 		/*
-		//Add change event handler to host file list
-		$('#hfm_list').change(function(){
-			self.updateFormElements();
+		//@TODO Validate path to system host file at some point
+		//Add blur event handler to hostfile field
+		$('#hfm_hostfile').bind('blur', function(){
+			self.updateHostFileName();
 		});
+		*/
+		//@TODO Add handlers for each piece of data change to update this.myPrefs
 		//Add click and keydown event handlers to save button
 		$('#hfm_save').bind('click keydown', function(){
 			self.saveData();
@@ -18,31 +23,50 @@ var prefs = {
 		$('#hfm_cancel').bind('click keydown', function(){
 			self.close();
 		});
-		//Add click and keydown event handlers to delete button
-		$('#hfm_delete').bind('click keydown', function(){
-			self.deleteHostFile();
-		});
-		//Add click and keydown event handlers to new button
-		$('#hfm_new').bind('click keydown', function(){
-			self.newHostFile();
-		});
-		//Add blur event handler to hostfile field
-		$('#hfm_hostfile').bind('blur', function(){
-			self.updateHostFileName();
-		});
-		//Add keypress event handler to hostfile field
-		$('#hfm_hostfile').bind('keyup', function(){
-			self.validateHostFileName();
-		});
-		//Add blur event handler to data field
-		$('#hfm_data').bind('blur', function(){
-			self.updateHostFileData();
-		});
 		//Got message from mother ship, handle it here
 		on('message', function(message){
 			self.handleMessage(message);
 		});
-		*/
+	},
+	
+	/**
+	 * Handle messages from the mother ship
+	 * @param message (mixed)
+	 */
+	handleMessage : function(message)
+	{
+		if (typeof message === 'object')
+		{
+			//We got the preferences
+			this.myPrefs = message;
+			this.updateForm();
+		}
+	},
+
+	updateForm : function()
+	{
+		//Build form data
+		$('#prefs_systemhostfilepath').val(this.myPrefs.systemHostFilePath);
+		if (this.myPrefs.refreshTab === true)
+		{
+			$('#prefs_refreshtab').attr('checked', 'checked');
+		}
+		else
+		{
+			$('#prefs_refreshtab').attr('checked', '');
+		}
+		$('#prefs_basehostfile').val(this.myPrefs.baseHostFile);
+	},
+
+	saveData : function()
+	{
+		//@TODO Check for escape key and dont save
+		postMessage(this.myPrefs);
+	},
+
+	close : function()
+	{
+		postMessage('close');
 	}
 };
 
